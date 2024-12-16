5d0d265aa9ec3820c5d8515d497b5c48e8dd5fd2
fcortesbio <fcortesbio@gmail.com>
2024-12-16 13:53:49 -0500

    updated views, added BS css styles and icons

--- app.js
+++ app.js
@@ -10,16 +10,24 @@ const port = 5000 || process.env.PORT;
 // MIDDLEWARES 
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json()); 
-// Static files 
-app.use(express.static("public"));  
+app.use(express.static("public"));  // Static files 
 // templating engines
 app.use(expressLayouts); 
-app.set("layout", "./layouts/main"); // create a layout to be reused
+app.set("layout", "./layouts/main");
 app.set("view engine", "ejs"); 
 
 // ROUTE HANDLERS
-    // Home 
-app.get("/", (req, res) => res.send("Hello, World!")); 
+// Home 
+app.get("/", (req, res) => {
+
+    const locals = {
+        title : "NodeJs",
+        description : "Free NodeJS User Management System"
+    }
+
+    res.render("index", locals)
+
+}); 
 
 app.listen(port, () => {
     console.log(`App listening on port '${port}'`)
